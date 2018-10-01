54b4ed6b5bf3a73999196cc5454c44955b2808e7
Cornelius Johnson <kyng.cornelius@outlook.com>
2018-09-30 20:48:55 -0400

    Changes w/FB configured & persisting data

--- app/src/main/java/com/agacecornish/sra/activities/Activity_Register.java
+++ app/src/main/java/com/agacecornish/sra/activities/Activity_Register.java
@@ -6,19 +6,27 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.agacecornish.sra.R;
 import com.agacecornish.sra.models.User;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class Activity_Register extends AppCompatActivity implements View.OnClickListener{
 
-    private TextView mName, mEmail, mPassword, mCpassword;
+    private EditText mName, mEmail, mPassword, mCpassword;
     Button registerButton, sign_In;
     private User regUser;
+
+    //temp
+    private static int regUserCounter = 0;
+
+    private DatabaseReference mDatabase;
 
     RelativeLayout screen1, btn, signIn;
     Handler handler = new Handler();
@@ -34,31 +42,39 @@ public class Activity_Register extends AppCompatActivity implements View.OnClick
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__register);
 
+        //getting instance of database
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
 
 
         screen1 = (RelativeLayout) findViewById(R.id.screen1);
 
         handler.postDelayed(runnable, 3000);
 
-        mName = (TextView) findViewById(R.id.name);
-        mEmail = (TextView) findViewById(R.id.email);
-        mPassword = (TextView) findViewById(R.id.password);
-        mCpassword = (TextView) findViewById(R.id.c_password);
+        mName =  findViewById(R.id.name);
+        mEmail =  findViewById(R.id.email);
+        mPassword =  findViewById(R.id.password);
+        mCpassword =  findViewById(R.id.c_password);
         registerButton = (Button) findViewById(R.id.btn);
         registerButton.setOnClickListener(this);
 
        // sign_In = (Button) findViewById(R.id.sign_in);
-
-
-
-
 
     }
 
     @Override
     public void onClick(View v) {
 
-        regUser = new User(mName.toString(), mEmail.toString(), mPassword.toString());
+        //creating user
+        regUser = new User(mName.getText().toString(), mEmail.getText().toString(),
+                mPassword.getText().toString());
+
+        //adding user to database
+        mDatabase.child("users").child("uID"+regUserCounter).setValue(regUser);
+
+        //increment do remove later
+        regUserCounter++;
+
         Log.d(regUser.toString(), "User Registered");
 
         Toast.makeText(v.getContext(), "Click here",
