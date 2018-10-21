2c5fb7c11f354484e7950757b174a9f8268c4e6f
Cornelius Johnson <kyng.cornelius@outlook.com>
2018-10-21 17:54:17 -0400

    added code to handle user w/firebase email & pwd auth

--- app/src/main/java/com/agacecornish/sra/activities/Activity_Register.java
+++ app/src/main/java/com/agacecornish/sra/activities/Activity_Register.java
@@ -1,8 +1,11 @@
 package com.agacecornish.sra.activities;
 
+import android.content.Intent;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +16,10 @@ import android.widget.Toast;
 
 import com.agacecornish.sra.R;
 import com.agacecornish.sra.models.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -27,6 +34,7 @@ public class Activity_Register extends AppCompatActivity implements View.OnClick
     private static int regUserCounter = 0;
 
     private DatabaseReference mDatabase;
+    private FirebaseAuth firebaseAuth;
 
     RelativeLayout screen1, btn, signIn;
     Handler handler = new Handler();
@@ -44,6 +52,9 @@ public class Activity_Register extends AppCompatActivity implements View.OnClick
 
         //getting instance of database
         mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        //getting instance of firebase authentication
+        firebaseAuth = FirebaseAuth.getInstance();
 
 
 
@@ -65,19 +76,47 @@ public class Activity_Register extends AppCompatActivity implements View.OnClick
     @Override
     public void onClick(View v) {
 
+        String authEmail = mEmail.getText().toString();
+        String authPwd = mPassword.getText().toString();
+
+        if(TextUtils.isEmpty(authEmail)){
+            Toast.makeText(getApplicationContext(),"Please fill in the required fields",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(authPwd) || authPwd.length() < 15){
+            Toast.makeText(getApplicationContext(),"Please fill in the required fields",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        firebaseAuth.createUserWithEmailAndPassword(authEmail,authPwd)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+                            Log.d(mName.toString(),"User Registered/Authenticated");
+                            //startActivity(new Intent(getApplicationContext(),Activity_Register.class));
+                            //finish();
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(),"E-mail or password is wrong",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
         //creating user
-        regUser = new User(mName.getText().toString(), mEmail.getText().toString(),
-                mPassword.getText().toString());
+        //regUser = new User(mName.getText().toString(), authEmail, authPwd);
 
         //adding user to database
-        mDatabase.child("users").child("uID"+regUserCounter).setValue(regUser);
+        //mDatabase.child("users").child("uID"+regUserCounter).setValue(regUser);
 
         //increment do remove later
-        regUserCounter++;
+        //regUserCounter++;
 
-        Log.d(regUser.toString(), "User Registered");
+        //Log.d(regUser.toString(), "User Registered");
 
-        Toast.makeText(v.getContext(), "Click here",
-                Toast.LENGTH_SHORT).show();
+        /*Toast.makeText(v.getContext(), "Click here",
+                Toast.LENGTH_SHORT).show();*/
     }
 }
